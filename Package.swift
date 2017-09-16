571c56597544ab5312d9bbb2e77c825dfefcdabb
Constantino Tsarouhas <ctxppc@gmail.com>
2017-09-16 21:00:00 +0200

    Updated for Swift PM 4.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
+// swift-tools-version:4.0
 // DepthKit © 2017 Constantino Tsarouhas
 
 import PackageDescription
 
 let package = Package(
-    name: "DepthKit"
+    name: "DepthKit",
+	products: [
+		.library(name: "DepthKit", targets: ["DepthKit"])
+	],
+	targets: [
+		.target(name: "DepthKit", path: "Sources"),
+		.testTarget(name: "DepthKit Tests", dependencies: ["DepthKit"], path: "Tests")
+	]
 )

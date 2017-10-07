0a089ab989c0eb6e01fb8865899afe73f554a6af
Constantino Tsarouhas <ctxppc@gmail.com>
2017-10-07 16:16:16 +0200

    Added optimised padding for arrays. Added unit tests for padding collections & arrays.

--- Sources/Collections/Padding on Range-Replaceable Collection.swift
+++ Sources/Collections/Padding on Range-Replaceable Collection.swift
@@ -6,6 +6,8 @@ extension RangeReplaceableCollection {
 	///
 	/// The collection isn't changed if `self` has fewer than `targetCount` elements.
 	///
+	/// - Complexity: O(*n* × `targetCount`), where *n* is the length of the collection.
+	///
 	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
 	///
 	/// - Parameter index: The index in `self` in which to insert padding.
@@ -13,13 +15,15 @@ extension RangeReplaceableCollection {
 	/// - Parameter targetCount: The length of the collection after padding.
 	public mutating func pad(at index: Index, with padding: Element, toCount targetCount: IndexDistance) {
 		while targetCount > count {
-			self.insert(padding, at: index)
+			insert(padding, at: index)	// FIXME: index may become invalid after the first insertion
 		}
 	}
 	
 	/// Returns a copy of `self` after inserting an element repeatedly at a location until `self` reaches a certain number of elements.
 	///
 	/// The collection is returned unchanged if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* × `targetCount`), where *n* is the length of the collection.
 	///
 	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
 	///
@@ -28,7 +32,7 @@ extension RangeReplaceableCollection {
 	/// - Parameter targetCount: The length of the collection after padding.
 	///
 	/// - Returns: A copy of `self` with zero or more insertions of `padding`.
-	public func padding(at index: Index, with padding: Element, toCount targetCount: IndexDistance) -> Self {
+	public func padded(at index: Index, with padding: Element, toCount targetCount: IndexDistance) -> Self {
 		var collection = self
 		collection.pad(at: index, with: padding, toCount: targetCount)
 		return collection
@@ -37,6 +41,8 @@ extension RangeReplaceableCollection {
 	/// Inserts a collection of elements repeatedly at a location in `self` until `self` reaches a certain number of elements.
 	///
 	/// If the difference in count isn't divible by the count of `padding`, the new count of `self` will exceed `targetCount`. The collection isn't changed if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* × `targetCount`), where *n* is the length of the collection.
 	///
 	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
 	/// - Requires: `padding` is a non-empty collection.
@@ -47,13 +53,15 @@ extension RangeReplaceableCollection {
 	public mutating func pad<C : Collection>(at index: Index, withContentsOf padding: C, toCount targetCount: IndexDistance) where C.Element == Element {
 		precondition(!padding.isEmpty, "Empty padding")
 		while targetCount > count {
-			self.insert(contentsOf: padding, at: index)
+			insert(contentsOf: padding, at: index)	// FIXME: index may become invalid after the first insertion
 		}
 	}
 	
 	/// Returns a copy of `self` after inserting a collection of elements repeatedly at a location until `self` reaches a certain number of elements.
 	///
 	/// If the difference in count isn't divible by the count of `padding`, the count of the returned collection will exceed `targetCount`. The collection is returned unchanged if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* × `targetCount`), where *n* is the length of the collection.
 	///
 	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
 	/// - Requires: `padding` is a non-empty collection.
@@ -63,10 +71,99 @@ extension RangeReplaceableCollection {
 	/// - Parameter targetCount: The length of the collection after padding.
 	///
 	/// - Returns: A copy of `self` with zero or more insertions of `padding`.
-	public func padding<C : Collection>(at index: Index, withContentsOf padding: C, toCount targetCount: IndexDistance) -> Self where C.Element == Element {
+	public func padded<C : Collection>(at index: Index, withContentsOf padding: C, toCount targetCount: IndexDistance) -> Self where C.Element == Element {
 		var collection = self
 		collection.pad(at: index, withContentsOf: padding, toCount: targetCount)
 		return collection
+	}
+	
+}
+
+extension Array {
+	
+	/// Inserts an element repeatedly at a location in `self` until `self` reaches a certain number of elements.
+	///
+	/// The array isn't changed if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* + `targetCount`), where *n* is the length of the array.
+	///
+	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
+	///
+	/// - Parameter index: The index in `self` in which to insert padding.
+	/// - Parameter padding: The element to insert repeatedly at `index`.
+	/// - Parameter targetCount: The length of the array after padding.
+	public mutating func pad(at index: Index, with padding: Element, toCount targetCount: IndexDistance) {
+		
+		let insertionCount = targetCount - count
+		guard insertionCount > 0 else { return }
+		
+		insert(contentsOf: repeatElement(padding, count: insertionCount), at: index)
+		
+	}
+	
+	/// Returns a copy of `self` after inserting an element repeatedly at a location until `self` reaches a certain number of elements.
+	///
+	/// The array is returned unchanged if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* + `targetCount`), where *n* is the length of the array.
+	///
+	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
+	///
+	/// - Parameter index: The index in `self` in which to insert padding.
+	/// - Parameter padding: The element to insert repeatedly at `index`.
+	/// - Parameter targetCount: The length of the array after padding.
+	///
+	/// - Returns: A copy of `self` with zero or more insertions of `padding`.
+	public func padded(at index: Index, with padding: Element, toCount targetCount: IndexDistance) -> Array {
+		var array = self
+		array.pad(at: index, with: padding, toCount: targetCount)
+		return array
+	}
+	
+	/// Inserts a collection of elements repeatedly at a location in `self` until `self` reaches a certain number of elements.
+	///
+	/// If the difference in count isn't divible by the count of `padding`, the new count of `self` will exceed `targetCount`. The array isn't changed if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* + `targetCount`), where *n* is the length of the array.
+	///
+	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
+	/// - Requires: `padding` is a non-empty collection.
+	///
+	/// - Parameter index: The index in `self` in which to insert padding.
+	/// - Parameter padding: The elements to insert repeatedly at `index`.
+	/// - Parameter targetCount: The length of the array after padding.
+	public mutating func pad<C : Collection>(at index: Index, withContentsOf padding: C, toCount targetCount: IndexDistance) where C.Element == Element {
+		
+		precondition(!padding.isEmpty, "Empty padding")
+		
+		let paddingElementCount = targetCount - count
+		guard paddingElementCount > 0 else { return }
+		
+		let (minimalInsertionCount, overshoot) = paddingElementCount.quotientAndRemainder(dividingBy: Int(padding.count))
+		let insertionCount = overshoot == 0 ? minimalInsertionCount : minimalInsertionCount + 1
+		
+		insert(contentsOf: repeatElement(padding, count: insertionCount).joined(), at: index)
+		
+	}
+	
+	/// Returns a copy of `self` after inserting a collection of elements repeatedly at a location until `self` reaches a certain number of elements.
+	///
+	/// If the difference in count isn't divible by the count of `padding`, the count of the returned array will exceed `targetCount`. The array is returned unchanged if `self` has fewer than `targetCount` elements.
+	///
+	/// - Complexity: O(*n* + `targetCount`), where *n* is the length of the array.
+	///
+	/// - Requires: `index` is an index to an element in `self`, or equal to `endIndex`.
+	/// - Requires: `padding` is a non-empty collection.
+	///
+	/// - Parameter index: The index in `self` in which to insert padding.
+	/// - Parameter padding: The elements to insert repeatedly at `index`.
+	/// - Parameter targetCount: The length of the array after padding.
+	///
+	/// - Returns: A copy of `self` with zero or more insertions of `padding`.
+	public func padded<C : Collection>(at index: Index, withContentsOf padding: C, toCount targetCount: IndexDistance) -> Array where C.Element == Element {
+		var array = self
+		array.pad(at: index, withContentsOf: padding, toCount: targetCount)
+		return array
 	}
 	
 }
--- Tests/Collection Padding Test Case.swift
+++ Tests/Collection Padding Test Case.swift
@@ -5,6 +5,149 @@ import XCTest
 
 class CollectionPaddingTestCase : XCTestCase {
 	
-	// TODO
+	func testLeadingPaddingArray() {
+		
+		let original = [1, 2, 3, 4]
+		
+		let unpadded = original.padded(at: original.startIndex, withContentsOf: [0, 0], toCount: 4)
+		XCTAssert(unpadded == original)
+		
+		let singleUnpadded = original.padded(at: original.startIndex, with: 0, toCount: 4)
+		XCTAssert(singleUnpadded == original)
+		
+		let evenPadded = original.padded(at: original.startIndex, withContentsOf: [0, 0], toCount: 10)
+		XCTAssert(evenPadded == [0, 0, 0, 0, 0, 0, 1, 2, 3, 4])
+		
+		let unevenPadded = original.padded(at: original.startIndex, withContentsOf: [0, 0], toCount: 11)
+		XCTAssert(unevenPadded == [0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4])
+		
+		let singlePadded = original.padded(at: original.startIndex, with: 0, toCount: 11)
+		XCTAssert(singlePadded == [0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4])
+		
+	}
+	
+	func testMiddlePaddingArray() {
+		
+		let original = [1, 2, 3, 4]
+		
+		let unpadded = original.padded(at: 1, withContentsOf: [0, 0], toCount: 4)
+		XCTAssert(unpadded == original)
+		
+		let singleUnpadded = original.padded(at: 1, with: 0, toCount: 4)
+		XCTAssert(singleUnpadded == original)
+		
+		let evenPadded = original.padded(at: 1, withContentsOf: [0, 0], toCount: 10)
+		XCTAssert(evenPadded == [1, 0, 0, 0, 0, 0, 0, 2, 3, 4])
+		
+		let unevenPadded = original.padded(at: 1, withContentsOf: [0, 0], toCount: 11)
+		XCTAssert(unevenPadded == [1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 3, 4])
+		
+		let singlePadded = original.padded(at: 1, with: 0, toCount: 10)
+		XCTAssert(singlePadded == [1, 0, 0, 0, 0, 0, 0, 2, 3, 4])
+		
+	}
+	
+	func testTrailingPaddingArray() {
+		
+		let original = [1, 2, 3, 4]
+		
+		let unpadded = original.padded(at: original.endIndex, withContentsOf: [0, 0], toCount: 4)
+		XCTAssert(unpadded == original)
+		
+		let singleUnpadded = original.padded(at: original.endIndex, with: 0, toCount: 4)
+		XCTAssert(singleUnpadded == original)
+		
+		let evenPadded = original.padded(at: original.endIndex, withContentsOf: [0, 0], toCount: 10)
+		XCTAssert(evenPadded == [1, 2, 3, 4, 0, 0, 0, 0, 0, 0])
+		
+		let unevenPadded = original.padded(at: original.endIndex, withContentsOf: [0, 0], toCount: 11)
+		XCTAssert(unevenPadded == [1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0])
+		
+		let singlePadded = original.padded(at: original.endIndex, with: 0, toCount: 10)
+		XCTAssert(singlePadded == [1, 2, 3, 4, 0, 0, 0, 0, 0, 0])
+		
+	}
+	
+	func testPaddingEmptyArray() {
+		
+		let unpadded = [].padded(at: 0, withContentsOf: [1, 1, 1], toCount: 0)
+		XCTAssert(unpadded == [])
+		
+		let singleUnpadded = [].padded(at: 0, with: 1, toCount: 0)
+		XCTAssert(singleUnpadded == [])
+		
+		let evenPadded = [].padded(at: 0, withContentsOf: [1, 1, 1], toCount: 6)
+		XCTAssert(evenPadded == [1, 1, 1, 1, 1, 1])
+		
+		let unevenPadded = [].padded(at: 0, withContentsOf: [1, 1, 1], toCount: 5)
+		XCTAssert(unevenPadded == [1, 1, 1, 1, 1, 1])
+		
+		let singlePadded = [].padded(at: 0, with: 1, toCount: 6)
+		XCTAssert(singlePadded == [1, 1, 1, 1, 1, 1])
+		
+	}
+	
+	func testLeadingPaddingString() {
+		
+		let original = "My unit tests pass! 😂"		// 21 characters
+		
+		let unpadded = original.padded(at: original.startIndex, withContentsOf: "🚨🚨", toCount: 21)
+		XCTAssert(unpadded == original)
+		
+		let evenPadded = original.padded(at: original.startIndex, withContentsOf: "🚨🚨", toCount: 25)
+		XCTAssert(evenPadded == "🚨🚨🚨🚨My unit tests pass! 😂")
+		
+		let unevenPadded = original.padded(at: original.startIndex, withContentsOf: "🚨🚨", toCount: 22)
+		XCTAssert(unevenPadded == "🚨🚨My unit tests pass! 😂")
+		
+		let singleUnpadded = original.padded(at: original.startIndex, with: "🚨", toCount: 21)
+		XCTAssert(singleUnpadded == original)
+		
+		let singlePadded = original.padded(at: original.startIndex, with: "🚨", toCount: 25)
+		XCTAssert(singlePadded == "🚨🚨🚨🚨My unit tests pass! 😂")
+		
+	}
+	
+	func testMiddlePaddingString() {
+		
+		let original = "My unit tests pass! 😂"		// 21 characters
+		
+		let unpadded = original.padded(at: original.index(of: "u")!, withContentsOf: "uu", toCount: 21)
+		XCTAssert(unpadded == original)
+		
+		let evenPadded = original.padded(at: original.index(of: "u")!, withContentsOf: "uu", toCount: 25)
+		XCTAssert(evenPadded == "My uuuuunit tests pass! 😂")
+		
+		let unevenPadded = original.padded(at: original.index(of: "u")!, withContentsOf: "uu", toCount: 22)
+		XCTAssert(unevenPadded == "My uuunit tests pass! 😂")
+		
+		let singleUnpadded = original.padded(at: original.index(of: "u")!, with: "u", toCount: 21)
+		XCTAssert(singleUnpadded == original)
+		
+		let singlePadded = original.padded(at: original.index(of: "u")!, with: "u", toCount: 25)
+		XCTAssert(singlePadded == "My uuuuunit tests pass! 😂")
+		
+	}
+	
+	func testTrailingPaddingString() {
+		
+		let original = "My unit tests pass! 😂"		// 21 characters
+		
+		let unpadded = original.padded(at: original.endIndex, withContentsOf: "😅😂", toCount: 21)
+		XCTAssert(unpadded == original)
+		
+		let evenPadded = original.padded(at: original.endIndex, withContentsOf: "😅😂", toCount: 25)
+		XCTAssert(evenPadded == "My unit tests pass! 😂😅😂😅😂")
+		
+		let unevenPadded = original.padded(at: original.endIndex, withContentsOf: "😅😂", toCount: 22)
+		XCTAssert(unevenPadded == "My unit tests pass! 😂😅😂")
+		
+		let singleUnpadded = original.padded(at: original.endIndex, with: "😂", toCount: 21)
+		XCTAssert(singleUnpadded == original)
+		
+		let singlePadded = original.padded(at: original.endIndex, with: "😂", toCount: 25)
+		XCTAssert(singlePadded == "My unit tests pass! 😂😂😂😂😂")
+		
+	}
 	
 }

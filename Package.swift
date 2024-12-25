54669e0fb68f2b84cdb95b1f6eaca6aec0bd7594
Constantino Tsarouhas <c@tsarouhas.eu>
2024-12-25 23:57:13 +0100

    Adds missing method.
    Signed-off-by: Constantino Tsarouhas <c@tsarouhas.eu>

--- Package.swift
+++ Package.swift
@@ -5,12 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "DepthKit",
+	platforms: [.macOS(.v15), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .visionOS(.v2)],
 	products: [
-		.library(name: "DepthKit", targets: ["DepthKit"])
+		.library(name: "DepthKit", targets: ["DepthKit"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMinor(from: "1.2.0")),
 	],
 	targets: [
-		.target(name: "DepthKit", path: "Sources"),
-		.testTarget(name: "DepthKitTests", dependencies: ["DepthKit"], path: "Tests")
+		.target(name: "DepthKit", dependencies: [
+			.product(name: "Algorithms", package: "swift-algorithms"),
+		]),
+		.testTarget(name: "DepthKitTests", dependencies: ["DepthKit"]),
 	],
 	swiftLanguageModes: [.v6]
 )

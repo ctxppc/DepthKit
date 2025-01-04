488bec9692db1f9adb3e64d5e4fc0b349e1f911a
Constantino Tsarouhas <c@tsarouhas.eu>
2025-01-04 13:22:45 +0100

    Fixes access issue.
    Signed-off-by: Constantino Tsarouhas <c@tsarouhas.eu>

--- Sources/DepthKit/Sequences/Empty Async Sequence.swift
+++ Sources/DepthKit/Sequences/Empty Async Sequence.swift
@@ -5,12 +5,20 @@
 /// An empty sequence can be of any element or failure type but never returns an element and never throws an error. The simplest empty sequence is typed `ElementSequence<Never, Never>`.
 public struct EmptyAsyncSequence<Element, Failure> : AsyncSequence {
 	
+	/// Creates an empty sequence.
+	public init() {}
+	
+	// See protocol.
 	public struct AsyncIterator : AsyncIteratorProtocol {
+		
+		// See protocol.
 		public func next() -> Element? {
 			nil
 		}
+		
 	}
 	
+	// See protocol.
 	public func makeAsyncIterator() -> AsyncIterator {
 		.init()
 	}
